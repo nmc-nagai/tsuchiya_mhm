73557175359c98c74d1d8ac85d685556db06da54
nmc_nagai <y-nagai@nm-co.jp>
2018-05-11 15:55:16 +0900

    mk def

---
+++ web/js/contents.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {
+
+    $('p.link.first, .back.last, #p4 .article img, #p1 .article img.third, .article3 .kasai img, .article3 .ryucheru img, .article3 div.title h1, .article3 div.title .item, .main_image h3, .article4 .title .cap_img, .article4 .title p, .article4 h3, .article5 .ryucheru, .article5 .kasai').css("opacity","0");
+    $('p.link.first').css("display","none");
+
+	$('#fullpage').fullpage({
+		// sectionSelector: '.page',
+		scrollOverflow: true,
+		afterRender: function(){
+			// $(def).css("opacity","0").css("transition","0s");
+			$("#p1 .article img.third").css("transition","0.7s").css("width","16%" );
+			$("#p1 .article img.third").css("opacity","1" );
+		},
+		// afterLoad: function(anchorLink, index){
+		// 	if( index == 4 ){
+		// 	//セクション１を下にスクロールしたとき実行されます
+		// 		// particlesJS();
+		// 		$("#p4 .article .top_img").css("opacity","1" );
+		// 	}
+		// },
+		onLeave: function(index, nextIndex, direction){
+			if(nextIndex == 1 ){
+				//セクション1へスクロールしたとき実行されます
+				// particlesJS();
+				// $("#p1 .article img.first, #p1 .article img.second").css("transition","0s").css("transition-delay","0.8s");
+				// $("#p1 .article img.first, #p1 .article img.second").css("opacity","1" );
+				$("#p1 .article img.third").css("transition","1.4s").css("transition-delay","0.7s").css("width","16%" );
+				$("#p1 .article img.third").css("opacity","1" );
+				// $("p.link.first").css("transition","0.7s").css("transition-delay","1.4s");
+				// $("p.link.first").css("display","block" );
+			}
+			if(index == 1 ){
+				//セクション1からスクロールしたとき実行されます
+				// particlesJS();
+				$("#p1 .article img").css("opacity","0").css("transition","0.5s");
+				$("#p1 .article img.third").css("width","23%" ).css("opacity","0").css("transition","0.5s");;
+				// $("p.link.first").css("display","none").css("opacity","0" ).css("transition","0.7s");
+
+			}
+			if(nextIndex == 2 ){
+				//セクション2へスクロールしたとき実行されます
+				// $(".next.first").css("opacity","0").css("transition","0.5s");
+				// $(".main_image h3").css("opacity","1").css("transition","0.7s").css("transition-delay","0.35s" );
+				// $(".article4 .title .cap_img, .article4 .title p, .article4 > h3").css("opacity","1").css("transition","0.7s").css("transition-delay","0.7s" );
+
+			}
+			if(index == 2 ){
+				//セクション2からスクロールしたとき実行されます
+				// $(".main_image h3, .article4 .title .cap_img, .article4 .title p, .article4 > h3").css("opacity","0").css("transition","0.5s");
+			}
+			if(nextIndex == 3 ){
+				//セクション3へスクロールしたとき実行されます
+				// $(".article5 .ryucheru").css("opacity","1").css("transition","0.7s").css("transition-delay","0.35s");
+				// $(".article5 .kasai").css("opacity","1").css("transition","0.7s").css("transition-delay","0.7s" );
+				// $(".back.last").css("opacity","1").css("transition","0.5s");
+				
+			}
+			if(index == 3 ){
+				//セクション3からスクロールしたとき実行されます
+				// $(".article5 .ryucheru, .article5 .kasai").css("opacity","0").css("transition","0.5s");
+				// $(".back.last").css("opacity","0").css("transition","0.5s");
+			}
+
+			if(nextIndex == 4 ){
+				//セクション4へスクロールしたとき実行されます
+				// particlesJS();
+				$("#p4 .article img").css("transition","0s");
+				$("#p4 .article img.first").css("transition","0.5s").css("transition-delay","0.2s");
+				$("#p4 .article img.second").css("transition-delay","0.6s");
+				$("#p4 .article img.third").css("transition-delay","1s");
+				$("#p4 .article img").css("opacity","1" );
+				$(".next.first").css("opacity","1").css("transition","0.5s");
+
+			}
+			if(index == 4 ){
+				//セクション4からスクロールしたとき実行されます
+				// particlesJS();
+				$("#p4 .article img").css("opacity","0").css("transition","0.5s");
+			}
+
+
+		},
+		afterLoad: function(anchorLink, index){
+			if(index == 1){
+				// $("p.link.first").css("opacity","1" ).css("transition","0.7s");
+			}
+		}
+	});
+});
+$(document).on('click', '#moveDown1, #moveDown2, #moveDown3', function(){
+	$.fn.fullpage.moveSectionDown();
+  });
+  $(document).on('click', '#moveTop', function(){
+	$.fn.fullpage.moveTo(1);
+  });
---
+++ web/js/fade.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.article1 .top_img,.article2 .kasai img, .article2 .ryucheru img, .article2 div.title h1, .article2 div.title .item').css("opacity","0");
+});
+
+
+  // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  // ga('create', 'UA-775394-6', 'auto');
+  // ga('send', 'pageview');
